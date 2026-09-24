deabbd1adf3842011602fd6bcc971a8d7628283b
lpdauto <lpdauto@gmail.com>
2026-09-24 12:31:20 -0700

    Match Hermes tested plugin registration pattern

--- __init__.py
+++ __init__.py
@@ -1,11 +1,5 @@
 """PRICING_V1 Hermes plugin registration."""
 
-from . import schemas, tools
-
 def register(ctx):
-    ctx.register_tool(
-        name="pricing_v1",
-        toolset="pricing_v1",
-        schema=schemas.PRICING_V1,
-        handler=tools.handle_pricing,
-    )
+    from .tools import register_tools
+    register_tools(ctx)
